8addfc969c9cd6f74a57e1687683f2dc461abdd1
koseyesim06 <koseyesim06@gmail.com>
2021-03-11 12:11:13 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey,TValue>
+    {
+        public TKey[] _keyArray;
+        public TValue[] _valueArray;
+       
+        public MyDictionary()
+        {
+            _keyArray = new TKey[0];
+            _valueArray = new TValue[0];
+        }
+
+        
+        public void Add(TKey key,TValue value)
+        {
+            TKey[] _temArray1= _keyArray;
+            TValue[] _temArray2=_valueArray;
+            _keyArray = new TKey[_keyArray.Length+1];
+            _valueArray = new TValue[_temArray2.Length + 1];
+
+            for (int i = 0; i < _temArray1.Length; i++)
+            {
+                _keyArray[i] = _temArray1[i];
+                
+            }
+            _keyArray[_keyArray.Length - 1] = key;
+
+            for (int j = 0; j < _temArray2.Length; j++)
+            {
+                
+                _valueArray[j] = _temArray2[j];
+            }
+            _valueArray[_valueArray.Length - 1] = value;
+
+
+        }
+        public int Count()
+        {
+             return _valueArray.Length; 
+        }
+        public void List()
+        {
+            for (int i = 0; i < _keyArray.Length; i++)
+            {
+                Console.WriteLine("MyDictionary Key: " + _keyArray[i] + "  MyDictionary Value: " + _valueArray[i]);
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //c# dictionary
+            var kullanicilar = new Dictionary<int, string>();
+            kullanicilar.Add(1, "Yeşim");
+            kullanicilar.Add(2, "Gülsün");
+
+
+            foreach (var kullanici in kullanicilar)
+            {
+                Console.WriteLine("Key :" + kullanici.Key + "   " + "Value : " + kullanici.Value);
+            }
+            Console.WriteLine("Count: " + kullanicilar.Count);
+            Console.WriteLine("\n");
+
+
+            //My Dictionary
+            MyDictionary<int,string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(134, "Tolga Demirer");
+            myDictionary.Add(158, "Ümit Özkan");
+            myDictionary.Add(115, "Kadir Aydemir");
+            myDictionary.Add(174, "Cemal Çiftçi");
+
+            Console.WriteLine("MyDictionary Count: "+myDictionary.Count());
+            myDictionary.List();
+
+        }
+    }
+}
